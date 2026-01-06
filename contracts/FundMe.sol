ee2fe782064144ddac0d2f7baa82d5665335a1f5
priscilla-nuel <prizzyllanwafor@gmail.com>
2026-01-06 22:55:48 +0100

    Fix Chainlink price feed call and decimal handling

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,10 +6,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from 
-"@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from
+ "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe  {
+
+    // to keep track of addresses we recieve funds from, we 
+    // create an array called funders
+    address[] public funder;
+
+    // to know the addresses that sent the funds and the amount
+    // they sent, in solidity you can also name your types
+
+
+    mapping(address => uint256) public 
+    addressToAmountFunded;
 
     //to specify the minimum usd to ve sent as $5, you 
     //need to declare a variavle and set it to $5 first
@@ -28,17 +39,29 @@ contract FundMe  {
         //if we want the user to spend money, we use 
         //require(msg.value)
 
-        require(msg.value >= 1e18, "no enought ETH"); //1e18 = 1eth = 1*10 **18
+        require(getConversionRate(msg.value) >= minimumUSD, 
+        "no enought ETH"); //1e18 = 1eth = 1*10 **18
+        funder.push(msg.sender);
+        addressToAmountFunded[msg.sender] = 
+        addressToAmountFunded[msg.sender] + msg.value;
     }
 
     // we need to create a function that would accurately get the current
     // price of any token that we would ve interacting with
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256) {
         //address : 0x694AA1769357215DE4FAC081bf1f309aDC325306
         AggregatorV3Interface dataFeed = AggregatorV3Interface
         (0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (int256 answer)= dataFeed.latestRoundData();
+       (,int256 answer,,,) = dataFeed.latestRoundData();
+    return uint256(answer * 1e18);
+    // the answer variavle here is the price and it will return the
+    // price of eth in terms of USD
+    // i got 3,219.91565700 as at the time i deployed this contract
+    // the dataFeed would always return 8 more digits than
+    // the original price, so you have to make them as decimals
+
+
         
     }
 
@@ -46,7 +69,15 @@ contract FundMe  {
     //we also need to create a function that get the conversion rate for the
     //tokens to usdc
 
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view
+     returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountinUSD = (ethPrice * ethAmount) / 1e36;
+        return ethAmountinUSD;
+
+
+
+    }
     //fuction that the owner will use to withdraw funds
     function withdraw() public {}
 
